ca37f12765b7ce8adcaf6b7ea92c5f98419da8af
NamrathaSriA6 <20a91a05a6@aec.edu.in>
2025-07-30 16:24:46 +0530

    Updated Angular version, added Firebase and TailwindCSS

---
+++ environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    apiKey: "AIzaSyCb7jKTcuqjkMRdzrLe0hDwtciHl-0_V3g",
+    authDomain: "retailproject-a27cf.firebaseapp.com",
+    databaseURL: "https://retailproject-a27cf-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "retailproject-a27cf",
+    storageBucket: "retailproject-a27cf.firebasestorage.app",
+    messagingSenderId: "248642238468",
+    appId: "1:248642238468:web:e5280878100098c2a39d51",
+    measurementId: "G-PQ5CJVZL5B"
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '../../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +25,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    AngularFireModule.initializeApp(environment),
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
